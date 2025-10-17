0079e40eaa488cd86acd0cd8c31a501f953881de
mecha-rahul <bishtrahul08022007@gmail.com>
2025-10-17 06:32:51 +0530

    Initia Commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+
+
+BG = pygame.transform.scale(pygame.image.load("background.jpg"),(WIDTH,HEIGHT))
+PLANET = pygame.transform.scale(pygame.image.load("Jupiter.png"),(PLANET_SIZE*2,PLANET_SIZE*2))
+
+WHITE =(255,255,255)
+RED=(255,0,0)
+BLUE=(0,0,255)
